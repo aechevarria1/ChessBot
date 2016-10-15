f27fc78fb34dffdd5222e4409ad09cd2498245d4
Scott Heng <scottheng3@gmail.com>
2016-10-15 00:51:45 -0400

    Created skeleton of pieces and chessboard
    still need to add moves and representation of board

--- Chess/New/ChessBoard.py
+++ Chess/New/ChessBoard.py
@@ -1,11 +1,113 @@
 import time,copy,random
+import Pieces
 
 class Board:
     def __init__(self):
-        pass
+        self.remainingWhitePieces = []
+        self.remainingBlackPieces = []
+        self.history = []
+        self.layout = {}
+        self.letters = "ABCDEFGH"
+        self.numbers = "12345678"
+        
+                for i in xrange(8):
+            self.rows.append(Row(i))
+            self.cols.append(Column(i))
+        self.remainingWhitePieces = []
+        self.remainingBlackPieces = []
+        
+        for i in xrange(8):
+            #Black Pawns
+            newPawn = Pieces.Pawn(self.letters[i]+"7", "Black", self)
+            self.remainingBlackPieces.append(newPawn)
+            self.layout[newPawn.getPosition()] = newPawn
+        
+        for i in xrange(8):
+            #White Pawns
+            newPawn = Pieces.Pawn(self.letters[i]+"2", "White", self)
+            self.remainingWhitePieces.append(newPawn)
+            self.layout[newPawn.getPosition()] = newPawn
+            
+        #Rooks
+        newRook = Pieces.Rook("A8","Black", self)
+        self.remainingBlackPieces.append(newRook)
+        self.layout[newRook.getPosition()] = newRook
+        
+        newRook = Pieces.Rook("H8","Black", self)
+        self.remainingBlackPieces.append(newRook)
+        self.layout[newRook.getPosition()] = newRook
+        
+        newRook = Pieces.Rook("A1","White", self)
+        self.remainingWhitePieces.append(newRook)
+        self.layout[newRook.getPosition()] = newRook
+        
+        newRook = Pieces.Rook("H1","White", self)
+        self.remainingWhitePieces.append(newRook)
+        self.layout[newRook.getPosition()] = newRook
+        
+        
+        #Knights
+        newKnight = Pieces.Knight("B8","Black", self)
+        self.remainingBlackPieces.append(newKnight)
+        self.layout[newKnight.getPosition()] = newKnight
+        
+        newKnight = Pieces.Knight("G8","Black", self)
+        self.remainingBlackPieces.append(newKnight)
+        self.layout[newKnight.getPosition()] = newKnight
+
+
+        newKnight = Pieces.Knight("B1","White", self)
+        self.remainingWhitePieces.append(newKnight)
+        self.layout[newKnight.getPosition()] = newKnight
+
+
+        newKnight = Pieces.Knight("G1","White", self)
+        self.remainingWhitePieces.append(newKnight)
+        self.layout[newKnight.getPosition()] = newKnight
+        
+        #Bishops
+        newBishop = Pieces.Bishop("C8","Black", self)
+        self.remainingBlackPieces.append(newBishop)
+        self.layout[newBishop.getPosition()] = newBishop
+        
+        newBishop = Pieces.Bishop("F8","Black", self)
+        self.remainingBlackPieces.append(newBishop)
+        self.layout[newBishop.getPosition()] = newBishop
+        
+        newBishop = Pieces.Bishop("C1","White", self)
+        self.remainingWhitePieces.append(newBishop)
+        self.layout[newBishop.getPosition()] = newBishop
+        
+        newBishop = Pieces.Bishop("F1","White", self)
+        self.remainingWhitePieces.append(newBishop)
+        self.layout[newBishop.getPosition()] = newBishop
+        
+        #Queens
+        newQueen = Pieces.Queen("D8","Black", self)
+        self.remainingBlackPieces.append(newQueen)
+        self.layout[newQueen.getPosition()] = newQueen
+        
+        newQueen = Pieces.Queen("D1","White", self)
+        self.remainingWhitePieces.append(newQueen)
+        self.layout[newQueen.getPosition()] = newQueen
+        
+        #Kings
+        newKing = Pieces.King("E8","Black", self)
+        self.remainingBlackPieces.append(newKing)
+        self.layout[newKing.getPosition()] = newKing
+        
+        newKing = Pieces.King("E1","White", self)
+        self.remainingWhitePieces.append(newKing)
+        self.layout[newKing.getPosition()] = newKing
+        
+        for letter in self.letters:
+            for number in self.numbers:
+                if letter+number not in self.layout.keys():
+                    self.layout(letter+number) = 0
+
     
     def __eq__(self, val):
-        pass
+        passs
     
     def __ne__(self,val):
         return not(self.__eq__(val))
@@ -14,6 +116,24 @@ class Board:
         pass
         
     def __str__(self):
+        return self.layout
+    
+    def resetBoard(self):
+        self.__init__()
+
+    def getPieceAt(self, position):
+        position = position.upper()
+        assert len(position) == 2
+        assert (position[0] in self.letter) and (position[1] in self.numbers)
+        return self.layout[position]
+
+    def getWhitePieces(self):
+        return self.remainingWhitePieces
+    def getBlackPieces(self):
+        return self.remainingBlackPieces
+    
+    def getAllRemainingPieces(self):
+        return self.remainingWhitePieces + self.remainingBlackPieces
+        
+    def saveGame(self):
         pass
-    
-    
--- Chess/New/Pieces.py
+++ Chess/New/Pieces.py
@@ -1,7 +1,108 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 11 22:29:58 2016
+class Piece(object):
+    def __init__(self, position, color, board):
+        position = position.upper()
+        assert  (length(position)==2) and (position[0] in "ABCDEFGH") and  (position[1] in "12345678")
+        self.position = position
+        color = color.upper()
+        assert (color == "WHITE" or color == "BLACK")
+        self.color = color
+        self.board = board
+        self.name = ""
+        self.hasMoved = 0
+        
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def __eq__(self, val):
+        """ Compare position of val to position of self. Return true if the two pieces have the same position. """
+        return val.getPosition()==self.getPosition()
+    
+    def __ne__(self, val):
+        return not(self.__eq__(val))
+        
+    def getPosition(self):
+        return self.position
+        
+    def setPosition(self, position):
+        position = position.upper()
+        assert (length(position)==2) and (position[0] in "ABCDEFGH") and  (position[1] in "12345678")
+        self.position = position
+            
+    def getColor(self):
+        return self.color
+        
+    def getName(self):
+        return self.name
+        
+    def getMoves(self):
+        return []
+    
+    getMoveStatus(self):
+        """
+        Used to determine if a pawn can double move or
+        if a king-rook pair can castle
+        """
+        return self.hasMoved   
 
-@author: arinz
-"""
 
+
+class King(Piece):
+    def __init__(self, position, color, board):
+        super(King, self).__init__(position, color, board)
+        self.name = color[0]+'K'
+    
+    def inCheck(self):
+        pass
+    def canQueenSideCastle(self):
+        pass
+    def canKingSideCastle(self):
+        pass
+    def getMoves(self):
+        pass
+
+class Queen(Piece):
+    def __init__(self, position, color, board):
+        super(Queen, self).__init__(position, color, board)
+        self.name = color[0]+'Q'
+
+    def getMoves(self):
+        pass
+    
+class Rook(Piece):
+    def __init__(self, position, color, board):
+        super(Rook, self).__init__(position, color, board)
+        self.name = color[0]+'R'
+        
+    def getMoves(self):
+        pass
+
+class Bishop(Piece):
+    def __init__(self, position, color, board):
+        super(Bishop, self).__init__(position, color, board)
+        self.name = color[0]+'B'
+        
+    def getMoves(self):
+        pass
+
+class Knight(Piece):
+    def __init__(self, position, color, board):
+        super(Knight, self).__init__(position, color, board)
+        self.name = color[0]+'N'
+    
+    def getMoves(self):
+        pass
+    
+class Pawn(Piece):
+    def __init__(self, position, color, board):
+        super(Pawn, self).__init__(position, color, board)
+        self.name = color[0]+'P'
+        self.lastPosition = position
+    
+    def getLastPosition(self):
+        return self.lastPosition
+        
+    def getMoves(self):
+        pass
