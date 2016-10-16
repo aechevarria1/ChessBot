609be989b6746f2ae42ced6e05e37cf70a0ceaa7
CentripetalA <arinze@mit.edu>
2016-10-16 17:28:33 -0400

    Created a Java implementation
    We will use bitboards to do this

---
+++ Chess/ChessJava/src/Chessboard.java
@@ -0,0 +1,14 @@
+
+public class Chessboard {
+	
+	//To make an instance of a new Chessboard
+	public Chessboard(){
+	
+	}
+	
+	//To make a Chessboard from previous game information
+	public Chessboard(String directory, String infoType){
+		
+	}
+
+}
