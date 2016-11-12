0bcda6486ff6a7baadf18a3ccf75c9025909a865
CentripetalA <arinze@mit.edu>
2016-11-12 02:18:35 -0500

    Frame for GUI

---
+++ Chess/ChessJava/src/ChessDebuggerGUI.java
@@ -0,0 +1,12 @@
+
+public class ChessDebuggerGUI {
+	// Make 8x8 grid for the game
+	// Make the top and left labels for file and rank
+	// Undo and Redo Buttons
+	// New game button
+	// Clear board button
+	// Build Board button
+	// Add specific pieces anywhere to the board
+	// Clicking on the piece shows available moves
+	// History of Moves Panel in the bottom
+}
